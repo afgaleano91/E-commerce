b196157b6b50f1b6c029497c5d1028b5cf06d3ec
Andres Galeano <afgaleano91@misena.edu.co>
2020-11-25 23:54:02 -0500

    Se modifica routes para el login

--- myshop/shop/admin/routes.py
+++ myshop/shop/admin/routes.py
@@ -1,13 +1,19 @@
 from flask import render_template, session, request, redirect, url_for, flash
 
 from shop import app, db, bcrypt
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import User
+from shop.products.models import Addproduct, Brand, Category
+
 import os
 
-@app.route('/')
-def home():
-    return render_template('admin/index.html',title='Admin page')
+@app.route('/admin')
+def admin():
+    if 'email' not in session:
+        flash(f"Please login first", "danger")
+        return redirect(url_for('login'))
+    products = Addproductquery.all()
+    return render_template('admin/index.html',title='Admin page', products=products)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -22,3 +28,18 @@ def register():
         flash(f'Welcome {form.name.data} Thanks for registering', 'Success')
         return redirect(url_for('home'))
     return render_template('admin/register.html', form=form, title="Register page")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email = form.email.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'Welcome {form.email.data} You are logedin now', 'success')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash("Wrong password please try again", 'danger')
+
+    return render_template('admin/login.html', form=form, title="Login page")
